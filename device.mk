ea7af5e66caa3ee085a3baf9d3c08f0f9381ca81
sbwml <984419930@qq.com>
2022-02-16 16:50:48 -0300

    bump vendor.display.config to 2.0
    * New WFD implementation needs vendor.display.confi@2.0.so

--- device.mk
+++ device.mk
@@ -182,6 +182,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
+	vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0
     vendor.display.config@1.1
 
 PRODUCT_COPY_FILES += \
