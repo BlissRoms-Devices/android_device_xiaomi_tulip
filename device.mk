9306e36ad9f6abfdbe957ab914bf6bc8e52a7017
MB <mb150996@gmail.com>
2022-02-16 16:29:36 -0300

    Build libcamera2ndk_vendor

--- device.mk
+++ device.mk
@@ -141,11 +141,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
 	Snap \
     libdng_sdk.vendor \
+	libcamera2ndk_vendor \
 	vendor.xiaomi.hardware.motor@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
