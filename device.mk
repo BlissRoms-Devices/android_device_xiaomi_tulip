bdfb91ad8f23497e2c470247955f810114db4972
matheucomth <matheussouzas65@outlook.com>
2022-02-18 10:21:03 -0300

    build vendor variant of vendor.display.config@1.11

--- device.mk
+++ device.mk
@@ -183,8 +183,9 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
 	vendor.display.config@1.0.vendor \
-    vendor.display.config@2.0
-    vendor.display.config@1.1
+    vendor.display.config@2.0 \
+    vendor.display.config@1.1 \
+	vendor.display.config@1.11.vendor 
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/display/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml \
