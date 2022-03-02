a12b60e174dda02b495ba05f8507f5bb6f327941
matheucomth <matheussouzas65@outlook.com>
2022-03-02 10:59:08 -0300

    switch to raven Feb Fp

--- bliss_tulip.mk
+++ bliss_tulip.mk
@@ -30,3 +30,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys
