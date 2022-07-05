0c2dfb3fb85565b3716ddb22061ccfc3666872dc
arturo51236 <rtsanonynousperson@gmail.com>
2022-07-05 11:20:23 -0300

    tulip: Switch to common IFAA
    * From LineageOS

--- device.mk
+++ device.mk
@@ -125,11 +125,9 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.tulip \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
+# IFAA
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    org.ifaa.aidl.manager
 
 # Bluetooth
 PRODUCT_PACKAGES += \
